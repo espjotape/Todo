1064ba6a26f864019952c3a230c2049a78d11c88
João Pedro <joaope.espindola2005@gmail.com>
2025-05-23 16:05:45 -0300

    Component: Button

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import PlusIcon from "./assets/plus.svg?react"
 import SpinnerIcon from "./assets/spinner.svg?react"
 import XIcon from "./assets/x.svg?react"
 import Badge from "./components/badge";
+import Button from "./components/button";
 
 export default function App() {
   return (
@@ -28,6 +29,10 @@ export default function App() {
         <div>
           <Badge variant="secondary">5</Badge>
           <Badge variant="primary">2 de 5</Badge>
+        </div>
+
+        <div>
+          <Button icon={PlusIcon}>Nova Tarefa</Button>
         </div>
       </div>
     </div>
