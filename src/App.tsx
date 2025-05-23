6a3e0b7aba16e43e480ffeef632b1efd5cbca06f
João Pedro <joaope.espindola2005@gmail.com>
2025-05-23 16:48:52 -0300

    Component: ButtonIcons

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import SpinnerIcon from "./assets/spinner.svg?react"
 import XIcon from "./assets/x.svg?react"
 import Badge from "./components/badge";
 import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
 
 export default function App() {
   return (
@@ -33,6 +34,11 @@ export default function App() {
 
         <div>
           <Button icon={PlusIcon}>Nova Tarefa</Button>
+        </div>
+        <div>
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant="secondary"/>
+          <ButtonIcon icon={TrashIcon} variant="tertiary"/>
         </div>
       </div>
     </div>
