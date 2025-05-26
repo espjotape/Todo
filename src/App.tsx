f87181b8c14e1e05a05750369abc2e5d0e184126
João Pedro <joaope.espindola2005@gmail.com>
2025-05-26 10:09:18 -0300

    Component: InputText

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,11 @@ import XIcon from "./assets/x.svg?react"
 import Badge from "./components/badge";
 import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
+import InputText from "./components/input-text";
 
 export default function App() {
   return (
-    <div className="grid gap-3">
+    <div className="grid gap-10">
       <div className="flex flex-col gap-2">
         <Text variant="body-sm-bold" className="text-red">Olá mundo!</Text>
         <Text className="text-green-base">Olá mundo!</Text>
@@ -39,6 +40,10 @@ export default function App() {
           <ButtonIcon icon={TrashIcon} />
           <ButtonIcon icon={TrashIcon} variant="secondary"/>
           <ButtonIcon icon={TrashIcon} variant="tertiary"/>
+        </div>
+
+        <div>
+          <InputText />
         </div>
       </div>
     </div>
