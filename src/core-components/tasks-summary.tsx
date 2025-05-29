b5e372b83bd2e2d467c41e7cfe913732d5b8faa3
João Pedro <joaope.espindola2005@gmail.com>
2025-05-29 17:03:50 -0300

    Updating Task Summary Badges

--- src/core-components/tasks-summary.tsx
+++ src/core-components/tasks-summary.tsx
@@ -1,20 +1,23 @@
 import Text from "../components/text"
 import Badge from "../components/badge"
+import useTasks from "../hooks/use-tasks"
 
 export default function TasksSummary() {
+ const { concludedTasksCount, tasksCount} = useTasks()
+
  return (
   <>
    <div className="flex items-center gap-2">
     <Text variant="body-sm-bold" className="!text-gray-300">
      Tarefas criadas
     </Text>
-    <Badge variant="secondary">5</Badge>
+    <Badge variant="secondary">{tasksCount}</Badge>
    </div>
    <div className="flex items-center gap-2">
     <Text variant="body-sm-bold" className="!text-gray-300">
      Concluídas
     </Text>
-    <Badge variant="primary">2 de 5</Badge>
+    <Badge variant="primary">{concludedTasksCount} de {tasksCount}</Badge>
    </div>
   </>
  )
