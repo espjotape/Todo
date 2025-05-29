e7820decdccceea31b04d08dab7b90e884721d0c
João Pedro <joaope.espindola2005@gmail.com>
2025-05-29 14:35:15 -0300

    Updating Task

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -10,6 +10,7 @@ import React, { useState } from "react";
 import InputText from "../components/input-text";
 import { Task, TaskState } from "../models/task";
 import { cx } from "class-variance-authority";
+import usePreparedTask from "../hooks/use-prepare-task";
 
 interface TaskItemProps {
  task: Task
@@ -20,7 +21,8 @@ export default function TaskItem({task} : TaskItemProps ) {
   task?.state === TaskState.Creating
  )
 
- const [taskTitle, setTaskTask] = useState("")
+ const [taskTitle, setTaskTask] = useState( task.title || "")
+ const { updateTask } = usePreparedTask()
 
  function handleEditTask() {
   setIsEditing(true)
@@ -36,7 +38,7 @@ export default function TaskItem({task} : TaskItemProps ) {
 
  function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
   e.preventDefault()
-  console.log({id: task.id, title: taskTitle})
+  updateTask(task.id, {title: taskTitle})
   setIsEditing(false)
  }
 
@@ -60,6 +62,7 @@ export default function TaskItem({task} : TaskItemProps ) {
    ) : (
     <form onSubmit={handleSaveTask} className="flex items-center gap-4" >
      <InputText 
+      value={taskTitle}
       className="flex-1"
       autoFocus
       required
